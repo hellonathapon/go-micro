d5ef359cd0146a141bd7ee631190e6511e6a4415
nathapon <hellonathapon@gmail.com>
2023-02-18 12:15:22 +0700

    feat(logger-service): Add logger service

---
+++ logger-service/cmd/api/handlers.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"logger-service/data"
+	"net/http"
+)
+
+type JSONPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+func (app *Config) WriteLog(w http.ResponseWriter, r *http.Request) {
+	// read json into var
+	var jsonPayload JSONPayload
+	_ = app.readJSON(w, r, &jsonPayload)
+
+	// insert data to logs
+	event := data.LogEntry{
+		Name: jsonPayload.Name,
+		Data: jsonPayload.Data,
+	}
+
+	err := app.Models.LogEntry.Insert(event)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	resp := jsonResponse{
+		Error:   false,
+		Message: "logged",
+	}
+
+	app.writeJSON(w, http.StatusAccepted, resp)
+
+}
